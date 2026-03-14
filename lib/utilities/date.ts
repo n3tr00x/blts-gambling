bc63a52b71510cc087783b0d24dc6b894e7d9f6c
Hubert Warchoł <n3tr00x99@gmail.com>
2026-03-15 00:11:55 +0100

    test: change format date function

--- lib/utilities/date.ts
+++ lib/utilities/date.ts
@@ -22,10 +22,34 @@ export function formatToLongMonthYear(dateString: string, locale = 'pl-PL') {
   return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
 }
 
-export const formatDateToISO = (date: Date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+// export const formatDateToISO = (date: Date) => {
+//   const year = date.getFullYear();
+//   const month = String(date.getMonth() + 1).padStart(2, '0');
+//   const day = String(date.getDate()).padStart(2, '0');
 
-  return `${year}-${month}-${day}`;
+//   return `${year}-${month}-${day}`;
+// };
+
+// export const formatDateToISO = (date: Date) => {
+//   const year = date.getUTCFullYear();
+//   const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+//   const day = String(date.getUTCDate()).padStart(2, '0');
+
+//   return `${year}-${month}-${day}`;
+// };
+
+// export const formatDateToISO = (date: Date) => {
+//   return date.toISOString().slice(0, 10);
+// };
+
+export const formatDateToISO = (date: Date) => {
+  const tzOffset = date.getTimezoneOffset() * 60000;
+  const correctedDate = new Date(date.getTime() - tzOffset);
+
+  return correctedDate.toISOString().slice(0, 10);
+};
+
+export const parseLocalDate = (date: string) => {
+  const [year, month, day] = date.split('-').map(Number);
+  return new Date(year, month - 1, day);
 };
